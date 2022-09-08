4b97a584377d773a4d8005af6b93e7773b618325
dfish19 <dfish19@yahoo.com>
2022-09-08 14:57:03 -0500

    initial

--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
 using System.Data;
+using System;
+using System.IO;
 
 var config = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
@@ -13,7 +15,7 @@ string connString = config.GetConnectionString("DefaultConnection");
 IDbConnection conn = new MySqlConnection(connString);
 
 var x = new DapperDepartmentRepository(conn);
-Console.WriteLine("What are you interested in?");
+Console.WriteLine("What department are you interested in?");
 var userInput = Console.ReadLine();
 x.InsertDepartment(userInput);
 var c = x.GetAllDepartments();
